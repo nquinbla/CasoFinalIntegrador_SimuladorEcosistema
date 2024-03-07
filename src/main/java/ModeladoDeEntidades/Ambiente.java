d0d8389f6bdaa7306c145d08c3c926b39e36f946
nquinbla <nquinbla@myuax.com>
2024-03-07 12:28:45 +0100

    a

---
+++ src/main/java/ModeladoDeEntidades/Ambiente.java
@@ -0,0 +1,76 @@
+package ModeladoDeEntidades;
+
+// [1] Clase Ambiente
+public class Ambiente {
+    private String clima; // Clima del ambiente
+    private String terreno; // Terreno del ambiente
+    private String recursos; // Recursos del ambiente
+
+    // [2] Constructor de la clase
+    public Ambiente(String clima, String terreno, String recursos) {
+        this.clima = clima;
+        this.terreno = terreno;
+        this.recursos = recursos;
+    }
+
+    // [3] Getters y Setters
+    public String getClima() {
+        return clima;
+    }
+
+public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public String getTerreno() {
+        return terreno;
+    }
+
+    public void setTerreno(String terreno) {
+        this.terreno = terreno;
+    }
+
+    public String getRecursos() {
+        return recursos;
+    }
+
+    public void setRecursos(String recursos) {
+        this.recursos = recursos;
+    }
+
+    // [4] Método toString
+    @Override
+    public String toString() {
+        return "Ambiente{" + "clima=" + clima + ", terreno=" + terreno + ", recursos=" + recursos + '}';
+    }
+
+    // [5] Método equals
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final Ambiente other = (Ambiente) obj;
+
+        if ((this.clima == null) ? (other.clima != null) : !this.clima.equals(other.clima)) {
+            return false;
+        }
+
+        if ((this.terreno == null) ? (other.terreno != null) : !this.terreno.equals(other.terreno)) {
+            return false;
+        }
+
+        if ((this.recursos == null) ? (other.recursos != null) : !this.recursos.equals(other.recursos)) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
