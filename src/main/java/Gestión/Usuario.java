62217c8a3b200d8311ca924a3e231bc5f42dfc5a
nquinbla <nquinbla@myuax.com>
2024-03-07 12:57:22 +0100

    siulador

--- src/main/java/Gestión/Usuario.java
+++ src/main/java/Gestión/Usuario.java
@@ -1,4 +1,27 @@
 package Gestión;
 
+// [1] Clase Usuario
 public class Usuario {
+    String nombre; // Nombre del usuario
+    String contraseña; // Contraseña del usuario
+
+    // [2] Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    // autenticar al usuario
+    public boolean autenticar(String nombre, String contraseña) {
+        return this.nombre.equals(nombre) && this.contraseña.equals(contraseña);
+    }
 }
