362c88bf184da7a8e9700d610bb8b914ad8ef18d
nquinbla <nquinbla@myuax.com>
2024-03-07 12:59:50 +0100

    usuario

--- src/main/java/Gestión/Registro.java
+++ src/main/java/Gestión/Registro.java
@@ -1,5 +1,32 @@
 package Gestión;
 import java.util.List
 
+// [1] Clase Registro
 public class Registro {
+    private List<String> entradas;
+    private List<String> salidas;
+
+    public void agregarEntrada(String entrada) {
+        entradas.add(entrada);
+    }
+    public void agregarSalida(String salida) {
+        salidas.add(salida);
+    }
+
+    // Setters y getters
+    public List<String> getEntradas() {
+        return entradas;
+    }
+
+    public List<String> getSalidas() {
+        return salidas;
+    }
+
+public void setEntradas(List<String> entradas) {
+        this.entradas = entradas;
+    }
+
+    public void setSalidas(List<String> salidas) {
+        this.salidas = salidas;
+    }
 }
