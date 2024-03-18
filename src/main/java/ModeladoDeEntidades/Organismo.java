64c9be0842f3f1a2d5a16d8c1c55e06a814728fb
nquinbla <nquinbla@myuax.com>
2024-03-18 16:29:11 +0100

    arreglos

--- src/main/java/ModeladoDeEntidades/Organismo.java
+++ src/main/java/ModeladoDeEntidades/Organismo.java
@@ -8,5 +8,62 @@ public abstract  class Organismo {
     protected int salud;
     protected boolean EstadoReproductivo;
 
+    // Constructor de la clase Organismo
+    public Organismo(int x, int y, int edad, int salud, boolean EstadoReproductivo) {
+        this.x = x;
+        this.y = y;
+        this.edad = edad;
+        this.salud = salud;
+        this.EstadoReproductivo = EstadoReproductivo;
+    }
+
+    // Getters y setters de la clase Organismo
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public boolean isEstadoReproductivo() {
+        return EstadoReproductivo;
+    }
+
+    public void setEstadoReproductivo(boolean EstadoReproductivo) {
+        this.EstadoReproductivo = EstadoReproductivo;
+    }
+
+    // Métodos Override
+    @Override
+    public String toString() {
+        return "Organismo{" + "x=" + x + ", y=" + y + ", edad=" + edad + ", salud=" + salud + ", EstadoReproductivo=" + EstadoReproductivo + '}';
+    }
+
+
+
 
 }
