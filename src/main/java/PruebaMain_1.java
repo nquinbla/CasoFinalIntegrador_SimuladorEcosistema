5e3f1036707b0b916ccad68cd9ad2a455177a684
nquinbla <nquinbla@myuax.com>
2024-03-18 20:59:25 +0100

    prueba main 1

---
+++ src/main/java/PruebaMain_1.java
@@ -0,0 +1,18 @@
+import AnálisisAvanzado.*;
+import GestiónUsuariosYSimulaciones.*;
+import ModeladoDeEntidades.*;
+import SimuladorDeDinámicas.*;
+
+import java.util.Scanner;
+
+
+public class PruebaMain_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
