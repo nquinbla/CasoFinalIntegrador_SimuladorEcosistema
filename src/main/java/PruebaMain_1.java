4b461ed3115dd39b36586931b03838e9c94ed279
nquinbla <nquinbla@myuax.com>
2024-03-18 21:02:25 +0100

    prueba main 1

--- src/main/java/PruebaMain_1.java
+++ src/main/java/PruebaMain_1.java
@@ -5,14 +5,44 @@ import SimuladorDeDinámicas.*;
 
 import java.util.Scanner;
 
-
 public class PruebaMain_1 {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
+        Simulador simulador = new Simulador();
 
+        while (true) {
+            System.out.println("------  MENÚ PRINCIPAL  ------");
+            System.out.println("Por favor, selecciona una opción:");
+            System.out.println("1. Agregar una nueva especie al ecosistema");
+            System.out.println("2. Simular X tiempo en el ecosistema");
+            System.out.println("3. Visualizar datos de la simulación");
+            System.out.println("4. Salir");
 
+            int opcion = scanner.nextInt();
 
-
+            switch (opcion) {
+                case 1:
+                    // Aquí puedes agregar la lógica para agregar una nueva especie al ecosistema
+                    break;
+                case 2: // Aquí puedes agregar la lógica para simular X tiempo en el ecosistema
+                    System.out.println("----- SIMULACIÓN DE TIEMPO  -----");
+                    System.out.println("Por favor, selecciona una opción:");
+                    System.out.println("1. Simular un día");
+                    System.out.println("2. Simular una semana");
+                    System.out.println("3. Simular un mes");
+                    System.out.println("4. Simular un año");
+                    System.out.println("5. Volver al menú principal");
+                    simulador.simularUnDia();
+                    break;
+                case 3:
+                    // Aquí puedes agregar la lógica para visualizar los datos de la simulación
+                    break;
+                case 4:
+                    System.out.println("Saliendo...");
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, intenta de nuevo.");
+            }
+        }
     }
 }
