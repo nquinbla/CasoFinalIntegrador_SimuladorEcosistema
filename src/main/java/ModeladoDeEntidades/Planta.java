31cd8a65da8a79549fef2d64c6af19388941f321
nquinbla <nquinbla@myuax.com>
2024-03-19 16:51:59 +0100

    arreglos

--- src/main/java/ModeladoDeEntidades/Planta.java
+++ src/main/java/ModeladoDeEntidades/Planta.java
@@ -5,14 +5,18 @@ public abstract class Planta extends Organismo {
     protected String nombre;
     protected String especie;
     protected int cantidadAgua;
+    protected int cantidadLuz;
+    protected int cantidadNutrientes;
 
 
     // Constructor de la clase Planta
-    public Planta(int x, int y, int edad, int salud, boolean EstadoReproductivo, String nombre, String especie, int cantidadAgua) {
+    public Planta(int x, int y, int edad, int salud, boolean EstadoReproductivo, String nombre, String especie, int cantidadAgua, int cantidadLuz, int cantidadNutrientes) {
         super(x, y, edad, salud, EstadoReproductivo);
         this.nombre = nombre;
         this.especie = especie;
         this.cantidadAgua = cantidadAgua;
+        this.cantidadLuz = cantidadLuz;
+        this.cantidadNutrientes = cantidadNutrientes;
     }
 
     // Getters y setters de la clase Planta
@@ -24,6 +28,12 @@ public abstract class Planta extends Organismo {
 
     public int getCantidadAgua() {return cantidadAgua;}
     public void setCantidadAgua(int cantidadAgua) {this.cantidadAgua = cantidadAgua;}
+
+    public int getCantidadLuz() {return cantidadLuz;}
+    public void setCantidadLuz(int cantidadLuz) {this.cantidadLuz = cantidadLuz;}
+
+    public int getCantidadNutrientes() {return cantidadNutrientes;}
+    public void setCantidadNutrientes(int cantidadNutrientes) {this.cantidadNutrientes = cantidadNutrientes;}
 
     // Método Override
     @Override
