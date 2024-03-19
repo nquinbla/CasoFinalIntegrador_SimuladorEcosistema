7ebe005a39ea3a6982c567216c582f4e9c2927ce
nquinbla <nquinbla@myuax.com>
2024-03-19 22:40:13 +0100

    arreglos organismos

--- src/main/java/ModeladoDeEntidades/Organismo.java
+++ src/main/java/ModeladoDeEntidades/Organismo.java
@@ -2,117 +2,62 @@ package ModeladoDeEntidades;
 
 // Clase organismo
 public abstract  class Organismo {
-    protected int posX;
-    protected int posY;
+    protected String nombreEspecie;
+    protected Posicion posicion;
     protected int edad;
     protected int salud;
     protected boolean EstadoReproductivo;
-    protected String nombreEspecie;
-    protected boolean esDepredador;
-    protected boolean esPresa;
-    protected boolean estaVivo;
 
     // Constructor de la clase Organismo
-    public Organismo(int x, int y, int edad, int salud, boolean EstadoReproductivo, String nombreEspecie, boolean esDepredador, boolean esPresa, boolean estaVivo) {
-        this.posX = x;
-        this.posY = y;
-        this.edad = 0; // edad inicial
-        this.salud = 100; // salud inicial
-        this.EstadoReproductivo = false; // no reproductivo al inicio
-        this.nombreEspecie = nombreEspecie;
-        this.esDepredador = esDepredador; // se asigna si es depredador o no
-        this.esPresa = esPresa; // se asigna si es presa o no
-        this.estaVivo = true; // el organismo está vivo al inicio
-    }
-
-    // Getters y setters de la clase Organismo
-    public int getPosX() {
-        return posX;
-    }
-
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-
-    public int getPosY() {
-        return posY;
-    }
-
-    public void setPosY(int posY) {
-        this.posY = posY;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
+    public Organismo(String nombreEspecie, Posicion posicion, int edad, int salud, boolean EstadoReproductivo) {
+        if (nombreEspecie == null || posicion == null) {
+            throw new IllegalArgumentException("Nombre y posición no pueden ser null");
+        }
+        this.nombreEspecie = nombreEspecie;;
+        this.posicion = posicion;
         this.edad = edad;
-    }
-
-    public int getSalud() {
-        return salud;
-    }
-
-    public void setSalud(int salud) {
         this.salud = salud;
-    }
-
-    public boolean isEstadoReproductivo() {
-        return EstadoReproductivo;
-    }
-
-    public void setEstadoReproductivo(boolean EstadoReproductivo) {
         this.EstadoReproductivo = EstadoReproductivo;
     }
 
-    public String getNombreEspecie() {
-        return nombreEspecie;
-    }
+    // Getters y setters de la clase Organismo
+    public String getNombre() {return nombreEspecie;}
+    public void setNombre(String nombre) {this.nombreEspecie = nombre;}
+    public int getEdad() {return edad;}
+    public void setEdad(int edad) {this.edad = edad;}
+    public int getSalud() {return salud;}
+    public void setSalud(int salud) {this.salud = salud;}
+    public boolean isEstadoReproductivo() {return EstadoReproductivo;}
+    public void setEstadoReproductivo(boolean EstadoReproductivo) {this.EstadoReproductivo = EstadoReproductivo;}
+    public String getNombreEspecie() {return nombreEspecie;}
+    public void setNombreEspecie(String nombreEspecie) {this.nombreEspecie = nombreEspecie;}
 
-    public void setNombreEspecie(String nombreEspecie) {
-        this.nombreEspecie = nombreEspecie;
-    }
 
-    public boolean isEsDepredador() {
-        return esDepredador;
-    }
 
-    public void setEsDepredador(boolean esDepredador) {
-        this.esDepredador = esDepredador;
-    }
+    // Método posición aleatoria
+    public static class Posicion {
+        private int x;
+        private int y;
 
-    public boolean isEsPresa() {
-        return esPresa;
-    }
+        public Posicion(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
 
-    public void setEsPresa(boolean esPresa) {
-        this.esPresa = esPresa;
-    }
+        public int getX() {
+            return x;
+        }
 
-    public boolean isEstaVivo() {
-        return estaVivo;
-    }
+        public void setX(int x) {
+            this.x = x;
+        }
 
-    public void setEstaVivo(boolean estaVivo) {
-        this.estaVivo = estaVivo;
-    }
+        public int getY() {
+            return y;
+        }
 
-    // Métodos de la clase Organismo
-    public void envejecer() {
-        this.edad++;
-        if (this.edad >= 5) { // Por ejemplo, consideremos que el estado reproductivo inicia a los 5 años
-            this.EstadoReproductivo = true;
+        public void setY(int y) {
+            this.y = y;
         }
     }
-
-    public void morir() {
-        this.estaVivo = false;
-        System.out.println("El organismo ha muerto en (" + this.posX + ", " + this.posY + ")");
-    }
-
-    public void interactuar(Ambiente ambiente) {
-        // Método para que los organismos interactúen con el ambiente
-    }
-
 }
