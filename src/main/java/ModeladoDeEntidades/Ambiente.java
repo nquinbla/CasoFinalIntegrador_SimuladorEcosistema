703e568204b196beac83a2502a1a93787f056d84
nquinbla <nquinbla@myuax.com>
2024-03-19 22:44:24 +0100

    arreglos ambiente

--- src/main/java/ModeladoDeEntidades/Ambiente.java
+++ src/main/java/ModeladoDeEntidades/Ambiente.java
@@ -16,10 +16,28 @@ public class Ambiente {
     }
 
     // Getters y Setters
-    public String getClima() {return clima;}
-    public void setClima(String clima) {this.clima = clima;}
-    public String getTerreno() {return terreno;}
-    public void setTerreno(String terreno) {this.terreno = terreno;}
-    public int getRecursosDisponibles() {return recursosDisponibles;}
-    public void setRecursosDisponibles(int recursosDisponibles) {this.recursosDisponibles = recursosDisponibles;}
+    public String getClima() {
+        return clima;
+    }
 
+    public void setClima(String clima) {
+        this.clima = clima;
+    }
+
+    public String getTerreno() {
+        return terreno;
+    }
+
+    public void setTerreno(String terreno) {
+        this.terreno = terreno;
+    }
+
+    public int getRecursosDisponibles() {
+        return recursosDisponibles;
+    }
+
+    public void setRecursosDisponibles(int recursosDisponibles) {
+        this.recursosDisponibles = recursosDisponibles;
+    }
+
+}
