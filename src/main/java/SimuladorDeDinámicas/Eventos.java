1efd5baf756ad2209219bbd661740645a4a983d4
nquinbla <nquinbla@myuax.com>
2024-03-19 17:17:31 +0100

    evento

--- src/main/java/SimuladorDeDinámicas/Eventos.java
+++ src/main/java/SimuladorDeDinámicas/Eventos.java
@@ -4,8 +4,13 @@ import ModeladoDeEntidades.*;
 import java.util.Random;
 import java.util.ArrayList;
 
+// Clase Eventos
 public abstract class Eventos {
     protected Random random = new Random();
 
+    // Método abstracto aplicarEvento
     public abstract void aplicarEvento(ArrayList<Organismo> poblacion);
+
 }
+
+
