af34b84bac4d64b69a798f0424676ead9c5dd9f0
nquinbla <nquinbla@myuax.com>
2024-03-19 21:05:49 +0100

    ajustes organismo

--- src/main/java/ModeladoDeEntidades/Planta.java
+++ src/main/java/ModeladoDeEntidades/Planta.java
@@ -4,84 +4,18 @@ import java.util.ArrayList;
 
 // Clase Planta
 public class Planta extends Organismo {
-    protected String nombre;
-    protected String especie;
-    protected int cantidadAgua;
-    protected int cantidadLuz;
-    protected int cantidadNutrientes;
 
-
-    // Lista de todas las plantas
-    public static ArrayList<Planta> todasLasPlantas = new ArrayList<>();
-
-    static {
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Rosa", "Rosa", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Girasol", "Helianthus annuus", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Orquídea", "Orchidaceae", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Margarita", "Bellis perennis", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Tulipán", "Tulipa", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Lirio", "Lilium", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Marihuana", "Cannabis sativa", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Coca", "Erythroxylum coca", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Amapola", "Papaver somniferum", 100, 100, 100));
-        todasLasPlantas.add(new Planta(2, 3, 24, 100, true, "Cactus", "Cactaceae", 100, 100, 100));
+    public Planta(int x, int y, int edad, int salud, boolean EstadoReproductivo, String nombreEspecie, boolean esDepredador, boolean esPresa, boolean estaVivo) {
+        super(x, y, edad, salud, EstadoReproductivo, nombreEspecie, esDepredador, esPresa, estaVivo);
     }
 
-    // Constructor de la clase Planta
-    public Planta(int x, int y, int edad, int salud, boolean EstadoReproductivo, String nombre, String especie, int cantidadAgua, int cantidadLuz, int cantidadNutrientes) {
-        super(x, y, edad, salud, EstadoReproductivo);
-        this.nombre = nombre;
-        this.especie = especie;
-        this.cantidadAgua = cantidadAgua;
-        this.cantidadLuz = cantidadLuz;
-        this.cantidadNutrientes = cantidadNutrientes;
-    }
-
-
-    // Getters y setters de la clase Planta
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEspecie() {
-        return especie;
-    }
-
-    public void setEspecie(String especie) {
-        this.especie = especie;
-    }
-
-    public int getCantidadAgua() {
-        return cantidadAgua;
-    }
-
-    public void setCantidadAgua(int cantidadAgua) {
-        this.cantidadAgua = cantidadAgua;
-    }
-
-    public int getCantidadLuz() {
-        return cantidadLuz;
-    }
-
-    public void setCantidadLuz(int cantidadLuz) {
-        this.cantidadLuz = cantidadLuz;
-    }
-
-    public int getCantidadNutrientes() {
-        return cantidadNutrientes;
-    }
-
-    public void setCantidadNutrientes(int cantidadNutrientes) {
-        this.cantidadNutrientes = cantidadNutrientes;
-    }
-
-    // Método Override
     @Override
-    public String toString() {
-        return nombre + " de la especie " + especie + " con " + salud + " de salud " + " y " + edad + " años de edad ";
+public void interactuar(Ambiente ambiente) {
+        this.salud += ambiente.obtenerRecursos(7); //
+        if (this.EstadoReproductivo && this.salud >= 75) {
+            Planta nuevaPlanta = new Planta(this.posX, this.posY, 0, 100, false, "Planta", false, false, true);
+            ambiente.agregarOrganismo(nuevaPlanta);
+            System.out.println("¡Una nueva planta ha brotado en (" + this.posX + ", " + this.posY + ")!");
+        }
     }
 }
