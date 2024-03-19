4133e6d8531f1990d39d8c449d947dd82edd8a9c
nquinbla <nquinbla@myuax.com>
2024-03-19 22:52:59 +0100

    arreglos y crecimiento

--- src/main/java/SimuladorDeDinámicas/Crecimiento.java
+++ src/main/java/SimuladorDeDinámicas/Crecimiento.java
@@ -1,33 +1,17 @@
 package SimuladorDeDinámicas;
 
-import java.util.ArrayList;
+import ModeladoDeEntidades.*;
 import java.util.List;
 
 public class Crecimiento {
-    private Especie especie;
-    private int cantidad;
 
-    public Crecimiento(Especie especie, int cantidad) {
-        this.especie = especie;
-        this.cantidad = cantidad;
+    public void crecimientoPoblacional(Organismo organismo, double factorCrecimiento) {
+        organismo.setSalud(organismo.getSalud() + (int)(organismo.getSalud() * factorCrecimiento));
     }
 
-    public Especie getEspecie() {
-        return especie;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void crecer() {
-        // Simulamos el crecimiento de la población según la tasa de crecimiento de la especie
-        cantidad += cantidad * especie.getTasaCrecimiento() / 100;
-    }
-
-    public void reproducirse() {
-        // Simulamos la reproducción de la población según la tasa de reproducción de la especie
-        cantidad += cantidad * especie.getTasaReproduccion() / 100;
+    public void calcularCrecimientoTotal(List<Organismo> organismos, double factorCrecimiento) {
+        for (Organismo organismo : organismos) {
+            crecimientoPoblacional(organismo, factorCrecimiento);
+        }
     }
 }
-
